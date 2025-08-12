2ad332559d112e522b10ac6561ea4155afb1c747
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-08-12 14:50:52 +0900

    refactor: 로그 양식 변경
    - 요청 스레드, 로그 레벨, 메시지, 시간, 로거 클래스 순서로 출력 양식 변경
    - ex) [http-nio-8080-exec-7] INFO  RESPONSE: client=[218.39.176.142:Mozilla/5.0 ...] 2025-08-12 05:36:14 fittoring.aspect.LogAspect

--- backend/fittoring/src/main/java/fittoring/aspect/LogAspect.java
+++ backend/fittoring/src/main/java/fittoring/aspect/LogAspect.java
@@ -82,7 +82,7 @@ public class LogAspect {
     @AfterThrowing(pointcut = "controller()", throwing = "e")
     public void afterThrowingController(JoinPoint joinPoint, Throwable e) {
         Method method = getMethod(joinPoint);
-        log.warn("[{}], [{}], [{}], [{}]", method.getName(), e.getClass() , e.getMessage(), e.getStackTrace());
+        log.warn("[{}], [{}], [{}], [{}]", e.getClass(), e.getMessage(), method.getName(), e.getStackTrace());
     }
 
     private Method getMethod(JoinPoint joinPoint) {
