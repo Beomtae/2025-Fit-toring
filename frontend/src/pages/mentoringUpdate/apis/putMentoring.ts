308be38dc9df2b9025fc77d5eb13afa420d859a0
kimyou1102 <yoosk5485@naver.com>
2025-08-14 16:03:05 +0900

    feat: putMentoring 함수 생성 #431

---
+++ frontend/src/pages/mentoringUpdate/apis/putMentoring.ts
@@ -0,0 +1,38 @@
+import { apiClient } from '../../../common/apis/apiClient';
+import { API_ENDPOINTS } from '../../../common/constants/apiEndpoints';
+
+import type { mentoringCreateFormData } from '../../../common/types/mentoringCreateFormData';
+
+interface PutMentoringParams {
+  mentoringData: mentoringCreateFormData;
+  profileImageFile: File | null;
+  certificateImageFiles: File[];
+  mentoringId: string;
+}
+
+export const putMentoring = async ({
+  mentoringData,
+  profileImageFile,
+  certificateImageFiles,
+  mentoringId,
+}: PutMentoringParams) => {
+  const formData = new FormData();
+
+  const jsonBlob = new Blob([JSON.stringify(mentoringData)], {
+    type: 'application/json',
+  });
+  formData.append('data', jsonBlob);
+
+  if (profileImageFile) {
+    formData.append('image', profileImageFile);
+  }
+  certificateImageFiles.forEach((file) =>
+    formData.append('certificateImages', file),
+  );
+
+  return apiClient.put({
+    endpoint: `${API_ENDPOINTS.MENTORINGS}/${mentoringId}`,
+    searchParams: { mentoringId },
+    withCredentials: true,
+  });
+};
