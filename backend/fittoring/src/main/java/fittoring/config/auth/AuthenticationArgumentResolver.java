b8bd60353d97126b99f02a77c493467f5a77ad7a
Lee Dong Yeop <s01054830455@gmail.com>
2025-08-16 22:20:15 +0900

    feat: resolver memberId null 체크 예외 구현

--- backend/fittoring/src/main/java/fittoring/config/auth/AuthenticationArgumentResolver.java
+++ backend/fittoring/src/main/java/fittoring/config/auth/AuthenticationArgumentResolver.java
@@ -20,10 +20,17 @@ public class AuthenticationArgumentResolver implements HandlerMethodArgumentReso
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
-                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
         HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
         String requestAttributeName = "memberId";
         Object memberId = request.getAttribute(requestAttributeName);
+        if (memberId == null) {
+            throw new NullPointerException("memberId가 null 입니다."); //403 or 500 논의
+            /*
+            403: 개발자의 실수로 AuthRequired를 안붙인 것은 맞지만, 클라이언트의 입장에서는 인가가 필요한 상황인데 인가가 되지 않은 상황이므로 401
+            500: 서버쪽에서 예상하지 못한 버그 같은 것들, 500으로 할거면 어떤 예외를 할지 고민
+             */
+        }
         return new LoginInfo((Long) memberId);
     }
 }
